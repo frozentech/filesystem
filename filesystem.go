5a6d38064114396761153e9168e3829f5527ddc6
Lester Soriano <lestersoriano@yahoo.com>
2021-01-22 10:21:15 +0800

    Added CreateFile

--- filesystem.go
+++ filesystem.go
@@ -1,6 +1,10 @@
 package filesystem
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 // Exist checks if folder or file exist
 func Exist(path string) (bool, error) {
@@ -16,7 +20,7 @@ func Exist(path string) (bool, error) {
 // Mkdir make directory if directory does not exist
 func Mkdir(path string) error {
 	if exist, _ := Exist(path); !exist {
-		return os.Mkdir(path, 0777)
+		return os.MkdirAll(path, 0777)
 	}
 	return nil
 }
@@ -24,4 +28,27 @@ func Mkdir(path string) error {
 // Delete delete file
 func Delete(filename string) error {
 	return os.Remove(filename)
+}
+
+// CreateFile write a config file
+func CreateFile(filename string, contents string) error {
+	directory := filepath.Dir(filename)
+
+	if exists, _ := Exist(directory); !exists {
+		if err := Mkdir(directory); err != nil {
+			return err
+		}
+	}
+
+	ext := filepath.Ext(filename)
+	filename = strings.Replace(filename, ext, "", 1)
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	file.WriteString(contents)
+	return nil
 }
